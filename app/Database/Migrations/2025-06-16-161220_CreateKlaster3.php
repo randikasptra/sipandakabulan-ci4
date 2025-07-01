7c14fa0a818010f97c5fd1372ab5cdd8757901be
randikasptra <saputrarandika857@gmail.com>
2025-07-01 21:04:02 +0700

    add/total-klaster3

--- app/Database/Migrations/2025-06-16-161220_CreateKlaster3.php
+++ app/Database/Migrations/2025-06-16-161220_CreateKlaster3.php
@@ -9,38 +9,38 @@ class CreateKlaster3 extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id'                          => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
-            'user_id'                     => ['type' => 'INT', 'unsigned' => true],
-            'tahun'                       => ['type' => 'INT', 'constraint' => 4],
-            'bulan'                       => ['type' => 'VARCHAR', 'constraint' => 20],
-            'status'                      => ['type' => 'ENUM', 'constraint' => ['pending', 'approved', 'rejected'], 'default' => 'pending'],
+            'id' => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'user_id' => ['type' => 'INT', 'unsigned' => true],
+            'tahun' => ['type' => 'INT', 'constraint' => 4],
+            'bulan' => ['type' => 'VARCHAR', 'constraint' => 20],
+            'status' => ['type' => 'ENUM', 'constraint' => ['pending', 'approved', 'rejected'], 'default' => 'pending'],
 
-            'kematianBayi'               => ['type' => 'INT', 'constraint' => 11],
-            'kematianBayi_file'          => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'kematianBayi' => ['type' => 'INT', 'constraint' => 11],
+            'kematianBayi_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
 
-            'giziBalita'                 => ['type' => 'INT', 'constraint' => 11],
-            'giziBalita_file'            => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'giziBalita' => ['type' => 'INT', 'constraint' => 11],
+            'giziBalita_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
 
-            'asiEksklusif'               => ['type' => 'INT', 'constraint' => 11],
-            'asiEksklusif_file'          => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'asiEksklusif' => ['type' => 'INT', 'constraint' => 11],
+            'asiEksklusif_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
 
-            'pojokAsi'                   => ['type' => 'INT', 'constraint' => 11],
-            'pojokAsi_file'              => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'pojokAsi' => ['type' => 'INT', 'constraint' => 11],
+            'pojokAsi_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
 
-            'pusatKespro'                => ['type' => 'INT', 'constraint' => 11],
-            'pusatKespro_file'           => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'pusatKespro' => ['type' => 'INT', 'constraint' => 11],
+            'pusatKespro_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
 
-            'imunisasiAnak'              => ['type' => 'INT', 'constraint' => 11],
-            'imunisasiAnak_file'         => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'imunisasiAnak' => ['type' => 'INT', 'constraint' => 11],
+            'imunisasiAnak_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
 
-            'layananAnakMiskin'          => ['type' => 'INT', 'constraint' => 11],
-            'layananAnakMiskin_file'     => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'layananAnakMiskin' => ['type' => 'INT', 'constraint' => 11],
+            'layananAnakMiskin_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
 
-            'kawasanTanpaRokok'          => ['type' => 'INT', 'constraint' => 11],
-            'kawasanTanpaRokok_file'     => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
-
-            'created_at'                 => ['type' => 'DATETIME', 'null' => true],
-            'updated_at'                 => ['type' => 'DATETIME', 'null' => true],
+            'kawasanTanpaRokok' => ['type' => 'INT', 'constraint' => 11],
+            'kawasanTanpaRokok_file' => ['type' => 'VARCHAR', 'constraint' => 255, 'null' => true],
+            'total_nilai' => ['type' => 'INT', 'null' => true],
+            'created_at' => ['type' => 'DATETIME', 'null' => true],
+            'updated_at' => ['type' => 'DATETIME', 'null' => true],
         ]);
 
         $this->forge->addKey('id', true);
